1dc9a5f94dd8d88e94731a415ff63c2e8376653b
ShouxTech <70824139+ShouxTech@users.noreply.github.com>
2023-04-18 22:26:37 -0400

    content

--- src/components/MajorProjects.jsx
+++ src/components/MajorProjects.jsx
@@ -1,23 +1,22 @@
 import Project from './Project';
-import OHBanner from '../assets/OHBanner.png'
-import OHLogo from '../assets/OHLogo.png'
-import AtlasBanner from '../assets/AtlasBanner.png'
-import AtlasLogo from '../assets/AtlasLogo.png'
-import RDBanner from '../assets/RDBanner.png'
-import RDLogo from '../assets/RDLogo.png'
-import COVBanner from '../assets/COVBanner.png'
-import COVLogo from '../assets/COVLogo.png'
+import OHBanner from '../assets/major-projects/OHBanner.png'
+import OHLogo from '../assets/major-projects/OHLogo.png'
+import AtlasBanner from '../assets/major-projects/AtlasBanner.png'
+import AtlasLogo from '../assets/major-projects/AtlasLogo.png'
+import RDBanner from '../assets/major-projects/RDBanner.png'
+import RDLogo from '../assets/major-projects/RDLogo.png'
+import COVBanner from '../assets/major-projects/COVBanner.png'
+import COVLogo from '../assets/major-projects/COVLogo.png'
 
 function MajorProjects(props) {
     return (
         <div>
             <p className="text-white text-center font-roboto font-bold text-5xl italic p-4">Major Projects</p>
-            {/* <div className="h-24"></div> */}
             <div className="grid m-20 gap-4 grid-cols-[repeat(auto-fill,19rem)] sm:grid-cols-[repeat(auto-fill,24rem)] justify-center">
-                <Project projectName="Rogue Demon" year="2022" bannerImage={RDBanner} icon={RDLogo}/>
-                <Project projectName="Atlas" year="2021" bannerImage={AtlasBanner} icon={AtlasLogo}/>
-                <Project projectName="City of Vancouver V1" year="2020" bannerImage={COVBanner} icon={COVLogo}/>
-                <Project projectName="Owl Hub" year="2019" bannerImage={OHBanner} icon={OHLogo}/>
+                <Project projectName="Rogue Demon" year="2022" desc="A battleground experience based on Demon Slayer. Over 100,000,000 visits and over 100,000 group members." url="https://www.roblox.com/games/9103898828/Rogue-Demon" bannerImage={RDBanner} icon={RDLogo}/>
+                <Project projectName="Atlas" year="2021" desc="An external (different from a script) software cheat for the Roblox client. Primarily universal aimbot and ESP." bannerImage={AtlasBanner} icon={AtlasLogo}/>
+                <Project projectName="City of Vancouver V1" year="2020" desc="A city experience. I was the main (and only) scripter for a year. The group has now moved on to V2 which I contributed to." url="https://www.roblox.com/groups/5530527/City-of-Vancouver-Canada" bannerImage={COVBanner} icon={COVLogo}/>
+                <Project projectName="Owl Hub" year="2019" desc="Widely known and used cheating script for a multitude of games on the Roblox platform." bannerImage={OHBanner} icon={OHLogo}/>
             </div>
         </div>
     )
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,8 @@
+import NewTab from '../assets/icons/NewTab.svg'
+
 function Project(props) {
     return (
-        <div className="w-full h-72 bg-zinc-700 rounded overflow-hidden shadow-lg">
+        <div className="w-full h-[335px] bg-zinc-700 rounded overflow-hidden shadow-lg relative">
             {/* Banner is 384x128 */}
             <div className="grid">
                 <img src={props.bannerImage} alt="Banner" className="bg-zinc-500 w-full h-32 blur-[2px] col-[1] row-[1]"></img> 
@@ -8,6 +10,13 @@ function Project(props) {
                 <img src={props.icon} alt="Icon" className="bg-zinc-800 w-20 h-20 rounded border z-10 col-[1] row-[1] border-zinc-500 p-2 ml-3 mt-[70px]"></img>
             </div>
             <h3 className="text-white font-roboto font-bold text-xl ml-3 mt-3">{props.projectName}</h3>
+            <p className="text-zinc-300 font-roboto pl-3 pr-3 mt-2">{props.desc}</p>
+            {props.url &&
+                <a className="ml-3 bottom-3 block absolute" target="_blank" href={props.url}>
+                    <img src={NewTab} className="w-[25px] h-[25px] inline"></img>
+                    <p className="inline ml-2 font-roboto text-zinc-400">Visit</p>
+                </a>
+            }
         </div>
     )
 }
