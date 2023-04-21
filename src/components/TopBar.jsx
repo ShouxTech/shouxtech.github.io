eba7493f8617d60a631be435f06fbd671cba6d12
ShouxTech <70824139+ShouxTech@users.noreply.github.com>
2023-04-21 10:32:52 -0400

    Update TopBar.jsx

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -8,13 +8,13 @@ function TopBar(props) {
         <div className="h-[70px] flex bg-[rgb(25,25,25)] items-center w-full fixed top-0 z-50">
             <CriShouxLogo className="w-[70px] h-[70px]"/>
             <h1 className="text-white font-product-sans font-bold text-2xl sm:text-3xl">CRISHOUX</h1>
-            <a href="https://www.roblox.com/users/57116727/profile" className="ml-auto mr-5">
+            <a href="https://www.roblox.com/users/57116727/profile" target="_blank" className="ml-auto mr-5">
                 <img src={Roblox} alt="Roblox" className="w-[25px] h-[25px] sm:w-[32px] sm:h-[32px]"></img>
             </a>
-            <a href="https://twitter.com/cri_shoux" className="mr-5">
+            <a href="https://twitter.com/cri_shoux" target="_blank" className="mr-5">
                 <img src={Twitter} alt="Twitter" className="w-[25px] h-[25px] sm:w-[32px] sm:h-[32px]"></img>
             </a>
-            <a href="https://www.youtube.com/crishoux" className="mr-5">
+            <a href="https://www.youtube.com/crishoux" target="_blank" className="mr-5">
                 <img src={YouTube} alt="YouTube" className="w-[25px] h-[25px] sm:w-[32px] sm:h-[32px]"></img>
             </a>
         </div>
