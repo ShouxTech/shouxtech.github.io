a604ff7e6af8b6fe83ceac53a283ded0b4b8713c
ShouxTech <70824139+ShouxTech@users.noreply.github.com>
2023-08-10 12:20:13 -0400

    Update TopBar.jsx

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -4,23 +4,32 @@ import Twitter from '../assets/icons/Twitter.svg'
 import Roblox from '../assets/icons/Roblox.svg'
 import GitHub from '../assets/icons/GitHub.svg'
 
+function Social(props) {
+    return (
+        <a href={props.url} target="_blank" className={`${props.className} mr-5`}>
+            <img src={props.imgSrc} alt={props.alt} className="w-[25px] h-[25px] sm:w-[32px] sm:h-[32px]"></img>
+        </a>
+    )
+}
+
 function TopBar(props) {
     return (
         <div className="h-[70px] flex bg-[rgb(25,25,25)] items-center w-full fixed top-0 z-50">
+            {/* Left margin. */}
+            <div className="w-0 sm:w-1/12"></div>
+
+            {/* Left side content. */}
             <CriShouxLogo className="w-[70px] h-[70px]"/>
             <h1 className="text-white font-product-sans font-bold text-2xl sm:text-3xl">CRISHOUX</h1>
-            <a href="https://www.roblox.com/users/57116727/profile" target="_blank" className="ml-auto mr-5">
-                <img src={Roblox} alt="Roblox" className="w-[25px] h-[25px] sm:w-[32px] sm:h-[32px]"></img>
-            </a>
-            <a href="https://github.com/ShouxTech" target="_blank" className="mr-5">
-                <img src={GitHub} alt="GitHub" className="w-[25px] h-[25px] sm:w-[32px] sm:h-[32px]"></img>
-            </a>
-            <a href="https://twitter.com/cri_shoux" target="_blank" className="mr-5">
-                <img src={Twitter} alt="Twitter" className="w-[25px] h-[25px] sm:w-[32px] sm:h-[32px]"></img>
-            </a>
-            <a href="https://www.youtube.com/crishoux" target="_blank" className="mr-5">
-                <img src={YouTube} alt="YouTube" className="w-[25px] h-[25px] sm:w-[32px] sm:h-[32px]"></img>
-            </a>
+
+            {/* Right side content. */}
+            <Social url="https://www.roblox.com/users/57116727/profile" imgSrc={Roblox} alt="Roblox" className="ml-auto"/>
+            <Social url="https://github.com/ShouxTech" imgSrc={GitHub} alt="GitHub"/>
+            <Social url="https://twitter.com/cri_shoux" imgSrc={Twitter} alt="Twitter"/>
+            <Social url="https://www.youtube.com/crishoux" imgSrc={YouTube} alt="YouTube"/>
+
+            {/* Right margin. */}
+            <div className="w-0 sm:w-1/12"></div>
         </div>
     )
 }
