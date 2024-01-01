ecec11d9050e2c8f66b8c9761bd7f6ecaf39ee81
ShouxTech <70824139+ShouxTech@users.noreply.github.com>
2023-12-31 23:30:32 -0500

    More details.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
+import React from 'react';
 import Contributions from './components/Contributions';
 import Copyright from './components/Copyright';
 import Header from './components/Header'
 import MajorProjects from './components/MajorProjects';
 import MinorProjects from './components/MinorProjects';
 import TopBar from './components/TopBar'
+import MoreInfo from './components/MoreInfo';
 
 function App() {
+    const [moreInfo, setMoreInfo] = React.useState();
+
     return (
         <div className="App">
             <TopBar/>
 
             <Header/>
-            <MajorProjects/>
-            <MinorProjects/>
-            <Contributions/>
+            <MajorProjects setMoreInfo={setMoreInfo}/>
+            <MinorProjects setMoreInfo={setMoreInfo}/>
+            <Contributions setMoreInfo={setMoreInfo}/>
+            {moreInfo && <MoreInfo setMoreInfo={setMoreInfo} {...moreInfo}/>}
 
             <Copyright/>
         </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,15 +2,15 @@ import NewTab from '../assets/icons/NewTab.svg'
 
 function Project(props) {
     return (
-        <div className="w-full h-[335px] bg-zinc-700 rounded overflow-hidden shadow-lg relative">
+        <div className="w-full h-[340px] bg-zinc-700 rounded overflow-hidden shadow-lg relative">
             {/* Banner is 384x128 */}
             <div className="grid">
-                <img src={props.bannerImage} alt="Banner" className="bg-zinc-500 w-full h-32 blur-[2px] col-[1] row-[1]"></img> 
+                <img src={props.bannerImage} alt="Banner" className="bg-zinc-500 w-full h-32 blur-[2px] col-[1] row-[1] object-cover"></img> 
                 <p className="text-white font-roboto rounded bg-gray-700 border border-zinc-500 z-10 w-fit h-fit p-1 col-[1] row-[1] justify-self-end mr-2 mt-2">{props.year}</p>
                 <img src={props.icon} alt="Icon" className="bg-zinc-800 w-20 h-20 rounded border z-10 col-[1] row-[1] border-zinc-500 p-2 ml-3 mt-[70px]"></img>
             </div>
             <h3 className="text-white font-roboto font-bold text-xl ml-3 mt-3">{props.projectName}</h3>
-            <p className="text-zinc-300 font-roboto pl-3 pr-3 mt-2">{props.desc}</p>
+            <p className="text-zinc-300 font-roboto pl-3 pr-3 mt-2">{props.desc}<span className='text-gray-400 cursor-pointer' onClick={() => props.setMoreInfo(props)}>{props.showMore ? ' Show more...' : ''}</span></p>
             {props.url &&
                 <a className="ml-3 bottom-3 block absolute" target="_blank" href={props.url}>
                     <img src={NewTab} className="w-[25px] h-[25px] inline"></img>
