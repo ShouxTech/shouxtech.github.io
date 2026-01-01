19032355397db936882b2651706aec0ca055fc12
ShouxTech <70824139+ShouxTech@users.noreply.github.com>
2025-12-31 21:42:51 -0500

    Update Copyright.jsx

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -2,7 +2,7 @@ function Copyright(props) {
     return (
         <div className='flex flex-col items-center'>
             <a href='mailto:crishoux@crishoux.com' className='text-zinc-500 font-roboto mb-1 text-sm hover:underline'>crishoux@crishoux.com</a>
-            <p className='text-zinc-500 font-roboto mb-2 text-sm'>© CriShoux 2025</p>
+            <p className='text-zinc-500 font-roboto mb-2 text-sm'>© CriShoux 2026</p>
         </div>
     );
 }
